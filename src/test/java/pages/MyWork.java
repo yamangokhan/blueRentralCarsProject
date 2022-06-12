3509fc156cf8892ac7d7cf793f594136ea93cdaf
Seher Yıldız <seheryildiz290@gmail.com>
2022-06-13 00:33:01 +0300

    Team08Project

---
+++ src/test/java/pages/MyWork.java
@@ -0,0 +1,25 @@
+package pages;
+
+public class MyWork {
+
+    /*
+   kullanıcı login butonu ile sayfaya girişi yapabilmeli verilen bilgiler ile giriş işlemi başarılı olmalı
+
+
+
+
+Kullanıcı ilgili sayfaya giriş yapar
+Kullanıcı anasayfanın https://www.bluerentalcars.com/ oldugunu test eder
+Kullanıcı sayfaya giriş aşamasında kısa süreli ekranda görülen sitenin logosunun varlıgını test eder
+Anasayfanın sağ üst köşesinde bulunan Login butonunu görülür ve tıklanabilir olmalı
+Kullanıcı Login sayfasında oldugunu test eder
+Email adres satırının tıklanabilir ve veri girişi yapılabilir olduğunu kullanıcı test eder.
+Password satırının tıklanabilir ve veri girişi yapılabilir olduğunu kullanıcı test eder.
+Login butonu görülebilir ve tıklanabilir olmalı
+Kullanıcı  verilen email ve password ile giriş yapabilmeli
+Kullanıcı verilen bilgiler ile giriş yaptğını doğrular
+
+*/
+
+
+}
