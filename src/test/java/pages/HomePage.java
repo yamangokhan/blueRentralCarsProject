de3d58febf0be38d8dea96ffc2bf1b8a8cff9fd0
sevil <sevilayturanuyanik@gmail.com>
2022-06-13 16:19:05 +0300

    13.06/16:20

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,35 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
-    //hello world deneme
+    //US_005 by Sevilay --
+
+    @FindBy(xpath = "//select[@name='car']")
+    public WebElement dropDown;
+    @FindBy(xpath = "//input[@name='pickUpLocation']")
+    public WebElement pickUpDropDown;
+    @FindBy(xpath = "//input[@title='Drop Off']")
+    public WebElement pickUpDropOff;
+    @FindBy(xpath = "//input[@name='pickUpDate']")
+    public WebElement pickUpDate;
+    @FindBy(xpath = "//input[@name='pickUpTime']")
+    public WebElement pickUpTime;
+    @FindBy(xpath = "//input[@name='dropOffDate']")
+    public WebElement dropOffDate;
+    @FindBy(xpath = "//input[@name='dropOffTime']")
+    public WebElement dropOffTime;
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement continueReservation;
+    @FindBy(xpath = "//*[text()='Please first login']")
+    public WebElement pleaseFirstLogin;
+
+
+
+
+
+
 }
+
+
